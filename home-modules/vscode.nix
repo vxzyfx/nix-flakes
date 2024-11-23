8b2f86e555d3bd406f685cf1a54e99f4babe1c9f
shug <vxzyfx@gmail.com>
2024-11-23 10:40:06 +0000

    fix: vscode linux wayland

--- home-modules/vscode.nix
+++ home-modules/vscode.nix
@@ -9,13 +9,16 @@ with lib;
 
 let
   cfg = config.home-modules.vscode;
+  defalutPackage = if vars.isLinux then (pkgs.vscode.override {
+      commandLineArgs = "--ozone-platform=wayland --enable-wayland-ime --gtk-version=4";
+    }) else pkgs.vscode;
 in {
   options.home-modules.vscode = {
     enable = mkEnableOption "vscode软件";
     package = mkOption {
       type = types.package;
       description = "安装的vscode软件包";
-      default = pkgs.vscode;
+      default = defalutPackage;
     };
   };
   config = mkIf cfg.enable {
