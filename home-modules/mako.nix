6ab53c3a249600be28ac5c5b8346647ddc083404
shug <vxzyfx@gmail.com>
2025-05-30 23:01:26 +0800

    fix: mako

--- home-modules/mako.nix
+++ home-modules/mako.nix
@@ -17,16 +17,18 @@ in
   config = mkIf cfg.enable {
     services.mako = {
       enable = true;
-      font = "monospace 10";
-      width = 350;
-      padding = "10";
-      margin = "10";
-      borderSize = 2;
-      borderRadius = 8;
-      backgroundColor = "#1d202f";
-      borderColor = "#f7768e";
-      textColor = "#c0caf5";
-      defaultTimeout = 3000;
+      settings = {
+        font = "monospace 10";
+        width = 350;
+        padding = "10";
+        margin = "10";
+        border-size = 2;
+        border-radius = 8;
+        background-color = "#1d202f";
+        border-color = "#f7768e";
+        text-color = "#c0caf5";
+        default-timeout = 3000;
+      };
     };
   };
 }
