3c2e2ba8959d8015948935df4af3a03cea9adeec
shug <vxzyfx@gmail.com>
2025-07-07 18:11:04 +0800

    update: shell node

--- shell/node.nix
+++ shell/node.nix
@@ -1,4 +1,17 @@
-{ pkgs, ... }: pkgs.mkShell {
-  packages = with pkgs; [ node2nix nodejs nodePackages.pnpm yarn ];
-  shellHook = "exec zsh";
+{ pkgs, vars, ... }:
+let
+  shell =
+    if pkgs.stdenv.hostPlatform.isDarwin then
+      "export SHELL=${pkgs.lib.getExe pkgs.zsh}; exec ${pkgs.lib.getExe pkgs.zsh}"
+    else
+      "";
+in
+pkgs.mkShell {
+  packages = with pkgs; [
+    node2nix
+    nodejs
+    nodePackages.pnpm
+    yarn
+  ];
+  shellHook = shell;
 }
