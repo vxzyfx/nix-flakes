e546d6873ce863e844d4f3998e79610ba713089a
shug <vxzyfx@gmail.com>
2025-07-18 11:13:14 +0800

    update: shell bun deno dotnet foundry swift

---
+++ shell/foundry.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+let
+  shell =
+    if pkgs.stdenv.hostPlatform.isDarwin then
+      "export SHELL=${pkgs.lib.getExe pkgs.zsh}; exec ${pkgs.lib.getExe pkgs.zsh}"
+    else
+      "";
+in
+pkgs.mkShell {
+  packages = with pkgs; [
+    foundry
+  ];
+  shellHook = shell;
+}
