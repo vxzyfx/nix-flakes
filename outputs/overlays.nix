98de6409527023bccd9d6c8a24430df006e9477e
shug <vxshug@gmail.com>
2026-04-24 23:06:20 +0800

    update: opencode v1.14.23

--- outputs/overlays.nix
+++ outputs/overlays.nix
@@ -6,6 +6,7 @@ let
     "sing-box"
     "kulala-ls"
     "kotlinls"
+    "opencode"
     "vscode-solidity-server"
   ];
   attrs = builtins.listToAttrs (
@@ -22,17 +23,17 @@ attrs
   #     patches = previousAttrs.patches ++ [ gdb-patche ];
   #   }
   # );
-  # neovim-unwrapped = prev.neovim-unwrapped.overrideAttrs (
-  #   finalAttrs: previousAttrs: {
-  #     version = "0.12.0";
-  #     src = final.fetchFromGitHub {
-  #       owner = "neovim";
-  #       repo = "neovim";
-  #       rev = "61f166ec409b7621fcc42e4da40d6ccb19973749";
-  #       hash = "sha256-JLAIrpQTp8u6blb5iZrAx36fNe65LT+JOUvxiLxlN+8=";
-  #     };
-  #   }
-  # );
+  neovim-unwrapped = prev.neovim-unwrapped.overrideAttrs (
+    finalAttrs: previousAttrs: {
+      version = "0.12.2";
+      src = final.fetchFromGitHub {
+        owner = "neovim";
+        repo = "neovim";
+        rev = "v${finalAttrs.version}";
+        hash = "sha256-V+jZiNv0SvG/GOOUPzmBkOQGrnrN3UW2BY2n9NxP2Eg=";
+      };
+    }
+  );
   tree-sitter26 = prev.rustPlatform.buildRustPackage (finalAttrs: {
     pname = "tree-sitter";
     version = "0.26.8";
@@ -56,21 +57,6 @@ attrs
         repo = "ollama";
         tag = "v${finalAttrs.version}";
         hash = "sha256-cAqc38NHvUo5gphq1csTyosTcpUjFcs0dzB0wreEGjs=";
-      };
-    }
-  );
-  opencode = prev.opencode.overrideAttrs (
-    finalAttrs: previousAttrs: {
-      version = "1.4.0";
-      patches = [ ];
-      src = final.fetchFromGitHub {
-        owner = "anomalyco";
-        repo = "opencode";
-        tag = "v${finalAttrs.version}";
-        hash = "sha256-u3OeU+3Y/O6KEeDiOl+pswBZ7++kMqwoK+ams03qWE4=";
-      };
-      node_modules = previousAttrs.node_modules.overrideAttrs {
-        outputHash = "sha256-atufNVv1pxdcz9TGhlZsQSwZ8E8dxJ7syPA/FD/cZWI=";
       };
     }
   );
