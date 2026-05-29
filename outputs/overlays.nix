9a8949444b500bd0b4c93b5a0290f4744915ffa5
shug <vxzyfx@gmail.com>
2026-05-29 20:49:23 +0800

    feat: remove overlay neovim opencode sing-box

--- outputs/overlays.nix
+++ outputs/overlays.nix
@@ -3,10 +3,7 @@ let
   callPackage = pname: final.callPackage ./packages/${pname}.nix { };
   packages = [
     "macism"
-    "sing-box"
-    "kulala-ls"
     "kotlinls"
-    "opencode"
     "vscode-solidity-server"
   ];
   attrs = builtins.listToAttrs (
@@ -23,26 +20,4 @@ attrs
   #     patches = previousAttrs.patches ++ [ gdb-patche ];
   #   }
   # );
-  neovim-unwrapped = prev.neovim-unwrapped.overrideAttrs (
-    finalAttrs: previousAttrs: {
-      version = "0.12.2";
-      src = final.fetchFromGitHub {
-        owner = "neovim";
-        repo = "neovim";
-        rev = "v${finalAttrs.version}";
-        hash = "sha256-V+jZiNv0SvG/GOOUPzmBkOQGrnrN3UW2BY2n9NxP2Eg=";
-      };
-    }
-  );
-  ollama = prev.ollama.overrideAttrs (
-    finalAttrs: previousAttrs: {
-      version = "0.17.7";
-      src = final.fetchFromGitHub {
-        owner = "ollama";
-        repo = "ollama";
-        tag = "v${finalAttrs.version}";
-        hash = "sha256-cAqc38NHvUo5gphq1csTyosTcpUjFcs0dzB0wreEGjs=";
-      };
-    }
-  );
 }
